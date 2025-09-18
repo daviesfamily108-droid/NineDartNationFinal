ea284fb119c66982d8a479cd05f3778d43bf1a04
daviesfamily108-droid <daviesfamily108@gmail.com>
2025-09-18 19:24:49 +0100

    main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,18 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
 import { AppProvider } from './state/store.jsx'
+import ProdErrorBoundary from './components/ProdErrorBoundary';
+
+// ...
+root.render(
+  <StrictMode>
+    <HashRouter>
+      <ProdErrorBoundary>
+        <App />
+      </ProdErrorBoundary>
+    </HashRouter>
+  </StrictMode>
+);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
